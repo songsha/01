3365b3d3cd705947aaeea1450838972fcdb0df19
crystal@joymason.cn <crystal@joymason.cn>
2016-11-01 17:47:50 +0800

    add mark

--- kvo/AppDelegate.m
+++ kvo/AppDelegate.m
@@ -14,7 +14,7 @@
 
 @implementation AppDelegate
 
-
+#pragma mark - lifecycle method
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;

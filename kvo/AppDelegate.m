839ae1e1abc9023d36c4f78c1a308d5da79341c9
ss <18363857227@163.com>
2016-11-02 17:11:39 +0800

    change rootview to viewcontroller

--- kvo/AppDelegate.m
+++ kvo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,8 +19,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor=[UIColor whiteColor];
     [self.window makeKeyWindow];
-    UIViewController *rootvc = [[UIViewController alloc] init];
+    ViewController *rootvc = [[ViewController alloc] init];
     self.window.rootViewController = rootvc;
     return YES;
 }
